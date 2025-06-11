f506a2b5a4122b8ba56146db753ead8ba2c4aa2d
Mehmet <107419555+BM-Mehmet@users.noreply.github.com>
2025-06-11 18:29:37 +0300

    Update connectdb.py

--- connectdb.py
+++ connectdb.py
@@ -4,7 +4,7 @@ def get_connection():
     db_config = {
         'host': 'localhost',
         'user': 'guvercin',
-        'password': '',
+        'password': '********',
         'database': 'guvercin'
     }
     db_connection = mysql.connector.connect(**db_config)
